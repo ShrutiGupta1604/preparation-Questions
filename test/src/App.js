43b795ab6b7c56333c8b7f9901cab38b43ceed3e
ShrutiGupta <ShrutiGupta1604@gmail.com>
2024-08-09 17:18:24 +0530

    Uraan Daily test create react app

--- test/src/App.js
+++ test/src/App.js
@@ -1,25 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
 
-export default App;
+//____________________________________________________________________________________________________
+
+// import React, { useState } from 'react';
+// function Digitalclock() {
+//     let currentTime = new Date().toLocaleTimeString();
+//     const[clocktime, setClocktime] = useState(currentTime )
+//     const Updatetime = () => {
+
+//         let time = new  Date().toLocaleTimeString();
+//         setClocktime(time)
+//     }
+//     setInterval(Updatetime,1000)
+//     return(
+//         <div>
+//             <h1>Digitalclock</h1>
+//             <h2>{clocktime}</h2>
+//         </div>
+//     )
+// }
+// export default Digitalclock;
+
+//_____________________________________________________________________________________________
+
+// import logo from './logo.svg';
+// import './App.css';
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
+
+// export default  App;
+
+//_________________________________________________________________________________________________________________
+ 
